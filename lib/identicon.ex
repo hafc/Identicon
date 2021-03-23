351450d27a58d39ff1e858b0b4c2d87db81e0462
Henrique Costa <hafc-dev@outlook.com>
2021-03-23 08:09:48 -0300

    Removendo numeros impares do grid de imagem

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -4,13 +4,23 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+   grid = Enum.filter(grid, fn {code, _index} ->
+      rem(code, 2) == 0
+    end)
+
+    %Identicon.Image{image | grid: grid}
   end
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
-    grid = Enum.chunk_every(hex, 3, 2, :discard)
-    |> Enum.map(&mirror_row/1)
-    |> List.flatten
-    |> Enum.with_index
+    grid =
+      Enum.chunk_every(hex, 3, 2, :discard)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten()
+      |> Enum.with_index()
 
     %Identicon.Image{image | grid: grid}
   end
@@ -26,8 +36,9 @@ defmodule Identicon do
   end
 
   def hash_input(input) do
-    hex = :crypto.hash(:md5, input)
-    |> :binary.bin_to_list
+    hex =
+      :crypto.hash(:md5, input)
+      |> :binary.bin_to_list()
 
     %Identicon.Image{hex: hex}
   end
